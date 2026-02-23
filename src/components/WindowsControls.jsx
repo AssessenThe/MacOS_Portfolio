9af43d20989601abb9ef25bba9e71c09c10c3bb0
AssessenThe <akc14919251@gmail.com>
2026-02-23 13:51:57 +0530

    implemented a hoc for managing window states and add the first window - terminal

--- src/components/WindowsControls.jsx
+++ src/components/WindowsControls.jsx
@@ -1,12 +1,27 @@
 import useWindowStore from "#store/window"
+import { X } from "lucide-react";
+import { RectangleHorizontal } from "lucide-react";
+import { Minus } from "lucide-react";
 
 const WindowsControls = ({ target}) => {
   const { closeWindow } = useWindowStore();
   return (
     <div id="window-controls">
-      <div className="close" onClick={() => closeWindow(target)} />
-      <div  className="minimize"/>
-      <div className="maximize" />
+      <div className="close" onClick={() => closeWindow(target)} >
+        <div className="close-button">
+          <X size={13} strokeWidth={3}/>
+        </div>
+      </div>
+      <div  className="minimize">
+        <div className="close-button">
+          <Minus size={13} strokeWidth={4}/>
+        </div>
+      </div>
+      <div className="maximize" >
+        <div className="close-button">
+          <RectangleHorizontal size={13} strokeWidth={4}/>
+        </div>
+      </div>
     </div>
   )
 }
